80eeb9e14418c963561738a3d0e8976aaa157771
Vishvesha V <104863039+wishwaysha@users.noreply.github.com>
2023-08-14 23:12:01 +0530

    solved error Resistor_colour_code.c
    Mistake was in the data type, for a few values, the int or float was not enough to hold it. Hence used long long int for all.
    improvements to be done - 
    1. Fractional resistances
    2. Tolerance band for all

--- Resistor_colour_code.c
+++ Resistor_colour_code.c
@@ -6,18 +6,19 @@
 
 char colours[12][10] = {"Black","Brown","Red","Orange","Yellow","Green","Blue","Violet","Grey","White","Gold","Silver"};
 char a;   
-float table[12][2];
-int i = 0,v1,v2,v3,m,t,score;
+long long int table[12][2];
+int i = 0,v1,v2,v3,m,t,score,tolerance,tol;
 char value[12];
-float res,actual_res,tolerance;
+unsigned long long int actual_res,res;
 
-int s_i(char x[12])
-{   int decimal=0;
+long long int s_i(char x[12])
+{   long long int decimal=0;
     for (int z = 0;z<strlen(x);z++)
     {
-        int r = pow(10,strlen(x) - z - 1);
+        long long int r = pow(10,strlen(x) - z - 1);
         decimal+= ((int)x[z] - 48)*r;
     }
+    //printf("returning decimal %lld\n",decimal);
     return decimal;
 }
 
@@ -32,39 +33,54 @@ int main()
     
    }
    
-   table[10][0] = pow(10,-1);
-   table[10][1] = 0.05;
-   table[11][0] = pow(10,-2);
-   table[11][1] = 0.1;
+  // table[10][0] = (float)pow(10,-1);
+   table[10][1] = 5.0;
+  // table[11][0] = (float)pow(10,-2);
+   table[11][1] = 10.0;
   // printf("%f  %f\n%f  %f\n",table[10][0],table[10][1],table[11][0],table[11][1]);
-   printf("\n\n\n\n__________ Type * to START/STOP the game_________\n");
+   printf("\n\n\n\n\t\t______________ Type * to START/STOP the game_________\n");
    scanf(" %c",&a);
    if( a  == '*')
    {   i = 0;score= 0;
-       printf("Guess the value of the resistor using the colour code\nType the value in the format <value> <tolerance>\t EG. 1000 5\n");
+       printf("\t\t\tGuess the value of the resistor using the colour code\n\t\t\tType the value in the format <value> <tolerance>\n\n\t\t\tEG. 1000 5   for 1k +- 5%\n");
        while(1)
        {   i++;
-           printf("Question no.%d\n",i);
+           printf("\n\n\nQuestion no.%d\n",i);
            v1 = rand()%10;
            v2 = rand()%10;
            v3 = rand()%10;
-           m = rand()%12;
-           t = (rand()%3) + 9;
-           printf("t = %d\n",t);
-           actual_res = ((v1*100)+(v2*10)+v3)*table[m][1];
-           tolerance = table[t][1]*100.0;
-           printf("table[t][1] = %f, table[t][1]*100.0= %d\n",table[t][1],table[t][1]*100.0);
+           m = rand()%10;
+           t = (rand()%2) + 10;
+         //  printf("t = %d\n",t);
+           actual_res = (long int)(((v1*100)+(v2*10)+v3)*(long long int)table[m][1]);
+         //  printf ("DEBUGGING-->(v1*100)+(v2*10)+v3) = %d,  (int)table[m][1]) = %lld \n ",(v1*100)+(v2*10)+v3,table[m][1]);
+           tolerance = (int)table[t][1];
+         //  printf("table[t][1] = %d\n",(int)table[t][1]);
            printf("%s %s %s %s %s\n",colours[v1],colours[v2],colours[v3],colours[m],colours[t]);
-           printf("actual_res = %f, tolerance = %d",actual_res,tolerance);
+         //  printf("actual_res = %lld, tolerance = %d\n",actual_res,tolerance);
            scanf(" %s",&value);
            if( value[0]  == '*')
            {
-               printf("Score = %d \n\n GAME TERMINATED",score); 
+               printf("\t\tScore = %d \n\n GAME TERMINATED",score); 
                return 0;
            }
            else res = s_i(value);
+           //printf("resistance is %lld\n",res);
+           scanf(" %d",&tol);
+           if(actual_res==res && tol == tolerance)
+           {
+               printf("\n________HURRRAAAYYY___________________________________________!!!!!\n");
+               score++;
+           }
+           else
+           {    
+               printf ("\n\tBBBBEEEEEPPPSSS, Wrong answer\n");
+               printf ("Actual answer --> RESISTANCE = %lld, TOLERANCE = %d\n",actual_res,tolerance);
+               printf("\t\tScore = %d \n\n GAME TERMINATED",score); 
+               return 0;
+           }
        }
    }
-   else printf("Inavalid start sequence, game terminated\n");
+   else printf("\tInavalid start sequence, game terminated\n");
    return 0;
 }
